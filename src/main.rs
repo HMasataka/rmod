4ee468d05141b13ee99ffbfb95b1b4b5f7a2c610
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-07-13 19:15:41 +0900

    Feat split file

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
-#[derive(Debug)]
-struct ID {
-    value: String,
-}
-impl ID {
-    fn new(value: &str) -> Self {
-        Self {
-            value: value.to_string(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Node {
-    id: ID,
-    label: String,
-}
-impl Node {
-    fn new(id: ID, label: &str) -> Self {
-        Self {
-            id,
-            label: label.to_string(),
-        }
-    }
-}
+mod id;
+mod node;
 
 fn main() {
-    let node = Node::new(ID::new("1"), "Node 1");
+    let node = node::Node::new(id::ID::new("1"), "Node 1");
 
     println!("Hello, module: {:?}", node);
 }
